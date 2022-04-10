98a7e7b75c02e9b5cc4c6e4602bed57d3b5a52bc
Katie Gabriel <47282619+kgabriel113@users.noreply.github.com>
2022-04-10 00:06:31 -0700

    basic prototype

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import CanvasDraw from "react-canvas-draw";
+
 
 function App() {
+  const [collecting, setCollecting] = React.useState(false);
+  var coordsList = [];
+
+  const windowRef = React.useRef(); // add window ref
+  // update window ref whenever window is updated
+  React.useEffect(function setupListeners() {
+
+    windowRef.current.addEventListener('mousemove', handleMove);
+    windowRef.current.addEventListener('mousedown', handleDown);
+    windowRef.current.addEventListener('mouseup', handleUp);
+
+    return function cleanupListeners() {
+      windowRef.current.removeEventListener('mousemove', handleMove);
+      windowRef.current.removeEventListener('mousedown', handleDown);
+      windowRef.current.removeEventListener('mouseup', handleUp);
+    }
+
+  }, [window, collecting]);
+
+  const handleDown = (_) => {
+    coordsList = [];
+    setCollecting(true);
+  }
+
+  const handleUp = (_) => {
+    console.log(coordsList)
+    setCollecting(false);
+  }
+
+  const handleMove = (event) => {
+    //if (collecting) console.log('beep')
+    if(collecting) coordsList.push({ x: event.offsetX, y: event.offsetY });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div ref={windowRef}>
+          <CanvasDraw />
+        </div>
       </header>
     </div>
   );
